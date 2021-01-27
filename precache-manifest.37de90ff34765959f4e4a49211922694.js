37dd7dba32d02488d9052bb941f22bc38ef8ae3e
Daria0109 <da6ulya888@gmail.com>
2021-01-27 20:48:59 +0300

    Updates

---
+++ precache-manifest.37de90ff34765959f4e4a49211922694.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98dc2823dba835ca51d6dbdd2dd05d0",
+    "url": "/Counter_ReactTS/index.html"
+  },
+  {
+    "revision": "dc47d428ed76b8867157",
+    "url": "/Counter_ReactTS/static/css/main.e8e65227.chunk.css"
+  },
+  {
+    "revision": "e14b95ab94dfee3ed68e",
+    "url": "/Counter_ReactTS/static/js/2.ad3f36fa.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Counter_ReactTS/static/js/2.ad3f36fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc47d428ed76b8867157",
+    "url": "/Counter_ReactTS/static/js/main.04b2d714.chunk.js"
+  },
+  {
+    "revision": "d414ca4aad5c90872a63",
+    "url": "/Counter_ReactTS/static/js/runtime-main.b5e92be6.js"
+  },
+  {
+    "revision": "add0c4244a015960586eeb51f091fd71",
+    "url": "/Counter_ReactTS/static/media/Orbitron-Regular.add0c424.ttf"
+  }
+]);
